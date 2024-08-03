2247455c6d169ef018d5cdd3d88353bba1330739
ranieriiuri <iuriranierioliveira@gmail.com>
2024-08-03 13:10:53 -0300

    Adding more files and modify calculator style

--- react-calculator-challenge/react-calculator-challenge/src/components/Button/index.js
+++ react-calculator-challenge/react-calculator-challenge/src/components/Button/index.js
@@ -1,9 +1,10 @@
+import { ButtonContainer } from "./styles";
 
-const Button = () => {
+const Button = ({label, onClick}) => {
     return (
-      <div className="App">
-        Olá
-      </div>
+      <ButtonContainer onClick={onClick}>
+        {label}
+      </ButtonContainer>
     );
   }
   
--- react-calculator-challenge/react-calculator-challenge/src/components/Button/styles.js
+++ react-calculator-challenge/react-calculator-challenge/src/components/Button/styles.js
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 2px solid #CDCDCD;
+    border-radius: 5px;
+    background-color: #00AAF0;
+    color: whitesmoke;
+    font-size: 24px;
+    font-weight: 700;
+    flex:1;
+
+`
